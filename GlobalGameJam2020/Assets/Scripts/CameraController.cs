d9a14b2bdee0ff0dac1bd9fa533d418dc3479c32
bruce <brunoandres89@outlook.com>
2020-02-01 18:49:13 -0300

    Added animations and particle system

---
+++ GlobalGameJam2020/Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        //PlayStartGameAnimation();
+    }
+
+    public void PlayStartGameAnimation()
+    {
+        animator.SetTrigger("StartGame");
+    }
+}
