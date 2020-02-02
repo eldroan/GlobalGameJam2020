9049d7a7cc819144be10fb63cabc9b9e990d42a5
Matias Luciano Cordoba <matias.cordoba@mercadolibre.com>
2020-02-02 02:00:07 -0300

    Commit de todo lo que hice hasta ahora

---
+++ GlobalGameJam2020/Assets/Director.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Director : MonoBehaviour
+{
+    public static Director Instance { get; private set;}
+
+    private void Awake() {
+        Instance = this;
+    }
+
+    public void Interact(string key)
+    {
+        switch(key) {
+            case "Atico":
+            break;
+            case "Madre":
+            break;
+            case "Living":
+            break;
+            case "Alma":
+            break;
+            case "Inventory":
+                Inventory.Instance.ShowHidde();
+            break;
+            case "Hermana":
+            break;
+        }
+    }
+}
